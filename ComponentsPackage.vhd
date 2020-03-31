a412ff3c1ccc856a3faaf9aae64d6c5757c0e586
YosefAhmed <youssefahmed.cis@gmail.com>
2020-03-31 21:16:07 +0200

    startup code

---
+++ ComponentsPackage.vhd
@@ -0,0 +1,32 @@
+--
+--	Package File Template
+--
+--	Purpose: This package defines supplemental types, subtypes, 
+--		 constants, and functions 
+--
+--   To use any of the example code shown below, uncomment the lines and modify as necessary
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+package ComponentsPackage is
+COMPONENT reg IS
+	GENERIC(n:NATURAL := 8);
+	PORT (
+	I: IN  STD_LOGIC_VECTOR (n-1 DOWNTO 0);
+	CLK, LOD, INC, CLR: IN STD_LOGIC;
+	O: OUT  STD_LOGIC_VECTOR (n-1 DOWNTO 0));
+END COMPONENT;
+end ComponentsPackage;
+
+--package body ComponentsPackage is
+
+--end ComponentsPackage;
---
+++ Reg_File.vhd
@@ -0,0 +1,23 @@
+
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.ComponentPackage.all;
+
+entity Reg_File is
+    Port ( readSel1 : in  STD_LOGIC_VECTOR(4 DOWNTO0);
+           readSel2 : in  STD_LOGIC_VECTOR(4 DOWNTO0);
+           writeSel : in  STD_LOGIC_VECTOR(4 DOWNTO0);
+           writeData : in  STD_LOGIC_VECTOR(31 DOWNTO 0);
+           writeEnable : in  STD_LOGIC;
+           dataOut1 : out  STD_LOGIC_VECTOR(31 DOWNTO 0);
+           dataOut2 : out  STD_LOGIC_VECTOR(31 DOWNTO 0));
+end Reg_File;
+
+architecture Behavioral of Reg_File is
+
+begin
+
+
+end Behavioral;
+
